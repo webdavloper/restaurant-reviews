a0e74bf5321d1fa2e1bb7cea7419f2082c7bab86
David Barbosa <davidsilva.sp@outlook.com>
2019-02-23 13:51:49 -0300

    service work changed

--- sw.js
+++ sw.js
@@ -5,16 +5,6 @@ const cacheUrls = [
   './manifest.json',
   './index.html',
   './restaurant.html',
-  './restaurant.html?id=1',
-  './restaurant.html?id=2',
-  './restaurant.html?id=3',
-  './restaurant.html?id=4',
-  './restaurant.html?id=5',
-  './restaurant.html?id=6',
-  './restaurant.html?id=7',
-  './restaurant.html?id=8',
-  './restaurant.html?id=9',
-  './restaurant.html?id=10',
   './css/styles.css',
   './data/restaurants.json',
   './js/dbhelper.js',
@@ -49,13 +39,13 @@ self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys()
       .then(cacheNames => {
-        cacheNames.filter(name => {
-          name.startsWith(('restaurant-') && name != staticCacheName)
+        return cacheNames.filter(name => {
+          return name.startsWith(('restaurant-') && name != staticCacheName)
         })
       })
       .then(cacheNames => Promise.all(cacheNames))
       .then(cacheNames => cacheNames.map(name => caches.delete(name)))
-      .catch(error => console.log('Kkkk'))
+      .catch(error => console.log(error))
   );
 });
 
